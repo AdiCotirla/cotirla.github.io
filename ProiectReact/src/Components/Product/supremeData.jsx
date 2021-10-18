19e6612417ae0eb86302e346d50058a6ec85697f
AdiCotirla <adioprea53@gmail.com>
2021-10-18 23:13:46 +0300

    meniu debug

--- ProiectReact/src/Components/Product/supremeData.jsx
+++ ProiectReact/src/Components/Product/supremeData.jsx
@@ -4,96 +4,96 @@ export const supremeData =
         categorie:"supreme",
         image:"https://cdn.shopify.com/s/files/1/0520/8974/9672/products/8c7b43fd_540x.jpg?v=1608247050",
         price: "300,00 lei",
-        name:"SUPREME HOODIE 1"
+        name:"HOODIE SUPREME PINK GOLD"
     },
     {
         categorie:"supreme",
-        image:"https://cdn.shopify.com/s/files/1/0520/8974/9672/products/3d6e02_47f5ae2de6354b659757e38f4afd1e53_mv2_540x.jpg?v=1608302251",
-        price: "200,00 lei",
-        name: "SUPREME HOODIE 2"
+        image:"https://cdn.shopify.com/s/files/1/0520/8974/9672/products/2020-12-18_16.40.53_540x.png?v=1608302596",
+        price: "199,00 lei",
+        name: "T-SHIRT SUPREME SC BLACK"
     },
     {
         categorie:"supreme",
-        image:"https://cdn.shopify.com/s/files/1/0520/8974/9672/products/3d6e02_47f5ae2de6354b659757e38f4afd1e53_mv2_540x.jpg?v=1608302251",
-        price: "200,00 lei",
-        name: "SUPREME HOODIE 2"
+        image:"https://cdn.shopify.com/s/files/1/0520/8974/9672/products/e9f857a9eadc67bf38ede02d84b173f9_540x.png?v=1608247496",
+        price: "699,00 lei",
+        name: "WINTER JACKET THE NORTHE FACE X SUPREME VLQ"
     },
     {
         categorie:"supreme",
-        image:"https://cdn.shopify.com/s/files/1/0520/8974/9672/products/3d6e02_47f5ae2de6354b659757e38f4afd1e53_mv2_540x.jpg?v=1608302251",
-        price: "200,00 lei",
-        name: "SUPREME HOODIE 2"
+        image:"https://cdn.shopify.com/s/files/1/0520/8974/9672/products/2bd9aa23_540x.jpg?v=1608246990",
+        price: "400,00 lei",
+        name: "HOODIE SUPREME GOLD"
     },
     {
         categorie:"supreme",
-        image:"https://cdn.shopify.com/s/files/1/0520/8974/9672/products/3d6e02_47f5ae2de6354b659757e38f4afd1e53_mv2_540x.jpg?v=1608302251",
-        price: "200,00 lei",
-        name: "SUPREME HOODIE 2"
+        image:"https://cdn.shopify.com/s/files/1/0520/8974/9672/products/f6140e2c_540x.jpg?v=1608247413",
+        price: "400,00 lei",
+        name: "JACKET THE NORTHE FACE X SUPREME LM"
     },
     {
         categorie:"supreme",
-        image:"https://cdn.shopify.com/s/files/1/0520/8974/9672/products/3d6e02_47f5ae2de6354b659757e38f4afd1e53_mv2_540x.jpg?v=1608302251",
-        price: "200,00 lei",
-        name: "SUPREME HOODIE 2"
+        image:"https://cdn.shopify.com/s/files/1/0520/8974/9672/products/supreme-x-the-north-face-2014-spring-summer-collection-7_540x.jpg?v=1608247644",
+        price: "500,00 lei",
+        name: "JACKET THE NORTHE FACE X SUPREME ATLAS"
     },
     {
         categorie:"supreme",
-        image:"https://cdn.shopify.com/s/files/1/0520/8974/9672/products/3d6e02_47f5ae2de6354b659757e38f4afd1e53_mv2_540x.jpg?v=1608302251",
-        price: "200,00 lei",
-        name: "SUPREME HOODIE 2"
+        image:"https://cdn.shopify.com/s/files/1/0520/8974/9672/products/supreme-x-the-north-face-2014-fall-winter-collection-9_540x.jpg?v=1608247349",
+        price: "400,00 lei",
+        name: "JACKET THE NORTHE FACE X SUPREME NINJA BLACK"
     },
     {
         categorie:"supreme",
-        image:"https://cdn.shopify.com/s/files/1/0520/8974/9672/products/3d6e02_47f5ae2de6354b659757e38f4afd1e53_mv2_540x.jpg?v=1608302251",
-        price: "200,00 lei",
-        name: "SUPREME HOODIE 2"
+        image:"https://cdn.shopify.com/s/files/1/0520/8974/9672/products/supreme-x-the-north-face-2014-fall-winter-collection-4_540x.jpg?v=1608247304",
+        price: "400,00 lei",
+        name: "JACKET THE NORTHE FACE X SUPREME NINJA RED"
     },
     {
         categorie:"supreme",
-        image:"https://cdn.shopify.com/s/files/1/0520/8974/9672/products/3d6e02_47f5ae2de6354b659757e38f4afd1e53_mv2_540x.jpg?v=1608302251",
-        price: "200,00 lei",
-        name: "SUPREME HOODIE 2"
+        image:"https://cdn.shopify.com/s/files/1/0520/8974/9672/products/3d6e02_be0e76e36a7a4cce9633f2a6a26650e7_mv2_540x.jpg?v=1608247225",
+        price: "199,00 lei",
+        name: "T-SHIRT SUPREME BOX LOGO R BL"
     },
     {
         categorie:"supreme",
-        image:"https://cdn.shopify.com/s/files/1/0520/8974/9672/products/3d6e02_47f5ae2de6354b659757e38f4afd1e53_mv2_540x.jpg?v=1608302251",
-        price: "200,00 lei",
-        name: "SUPREME HOODIE 2"
+        image:"https://cdn.shopify.com/s/files/1/0520/8974/9672/products/31_540x.jpg?v=1619335900",
+        price: "500,00 lei",
+        name: "JACKET THE NORTHE FACE X SUPREME"
     },
     {
         categorie:"supreme",
-        image:"https://cdn.shopify.com/s/files/1/0520/8974/9672/products/3d6e02_47f5ae2de6354b659757e38f4afd1e53_mv2_540x.jpg?v=1608302251",
-        price: "200,00 lei",
-        name: "SUPREME HOODIE 2"
+        image:"https://cdn.shopify.com/s/files/1/0520/8974/9672/products/2020-12-18_16.41.05_720x.png?v=1608302621",
+        price: "199,00 lei",
+        name: "T-SHIRT SUPREME X JORDAN BLACK"
     },
     {
         categorie:"supreme",
-        image:"https://cdn.shopify.com/s/files/1/0520/8974/9672/products/3d6e02_47f5ae2de6354b659757e38f4afd1e53_mv2_540x.jpg?v=1608302251",
-        price: "200,00 lei",
-        name: "SUPREME HOODIE 2"
+        image:"https://cdn.shopify.com/s/files/1/0520/8974/9672/products/2020-12-18_16.45.14_720x.png?v=1608302745",
+        price: "199,00 lei",
+        name: "T-SHIRT SUPREME X JORDAN WHITE"
     },
     {
         categorie:"supreme",
-        image:"https://cdn.shopify.com/s/files/1/0520/8974/9672/products/3d6e02_47f5ae2de6354b659757e38f4afd1e53_mv2_540x.jpg?v=1608302251",
-        price: "200,00 lei",
-        name: "SUPREME HOODIE 2"
+        image:"https://cdn.shopify.com/s/files/1/0520/8974/9672/products/8_720x.jpg?v=1619771783",
+        price: "500,00 lei",
+        name: "JACKET THE NORTHE FACE X SUPREME WQ"
     },
     {
         categorie:"supreme",
-        image:"https://cdn.shopify.com/s/files/1/0520/8974/9672/products/3d6e02_47f5ae2de6354b659757e38f4afd1e53_mv2_540x.jpg?v=1608302251",
+        image:"https://cdn.shopify.com/s/files/1/0520/8974/9672/products/2021-01-10_11.38.29_720x.png?v=1610297742",
         price: "200,00 lei",
-        name: "SUPREME HOODIE 2"
+        name: "SUPREME BAG"
     },
     {
         categorie:"supreme",
-        image:"https://cdn.shopify.com/s/files/1/0520/8974/9672/products/3d6e02_47f5ae2de6354b659757e38f4afd1e53_mv2_540x.jpg?v=1608302251",
-        price: "200,00 lei",
-        name: "SUPREME HOODIE 2"
+        image:"https://cdn.shopify.com/s/files/1/0520/8974/9672/products/2021-01-10_11.39.08_540x.png?v=1610297891",
+        price: "250,00 lei",
+        name: "TNF X SUPREME BAG"
     },
     {
         categorie:"supreme",
-        image:"https://cdn.shopify.com/s/files/1/0520/8974/9672/products/3d6e02_47f5ae2de6354b659757e38f4afd1e53_mv2_540x.jpg?v=1608302251",
-        price: "200,00 lei",
-        name: "SUPREME HOODIE 2"
+        image:"https://cdn.shopify.com/s/files/1/0520/8974/9672/products/2021-01-10_19.00.48_720x.png?v=1610298087",
+        price: "250,00 lei",
+        name: "TNF X SUPREME BACKPACK"
     }
 ]
